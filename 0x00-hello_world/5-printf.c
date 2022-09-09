918f9dceb8c09283d7c8bdcc6a28aca99e24071c
Aitsebaomo Jerry <jerrickson31@gmail.com>
2022-09-09 14:57:23 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
-#include <stdio .h>
+#include <stdio.h>
 
 /**
- * main - A program that prints certain strings to the standard output
+ * main - print output
  *
  * Return: Always 0 on success
  */
-
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,")
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
