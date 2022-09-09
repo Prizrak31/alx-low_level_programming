628e23d19c450b12a36da784c5b3b0059fbe593d
Aitsebaomo Jerry <jerrickson31@gmail.com>
2022-09-09 15:34:30 -0700

    script 4 fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,4 +2,11 @@
 
 /**
  * main - print the string in the put function
- * It prints "programming is like building a mu
+ *
+ * Return: Always return 0 on success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
